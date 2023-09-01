5c3e91f70364420cf5b7fbe2ff603956c7cbd677
alldevfs <alldevfs@outlook.com>
2023-08-31 21:11:40 -0300

    primeiro versão projeto r3s

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.calc').addEventListener('click', () => {
+    const firstValue = document.querySelector('.firstvalue')
+    const seconValue = document.querySelector('.secondvalue')
+    const thirdValue = document.querySelector('.thirdValue')
+    const forthValue = document.querySelector('.forthValue')
+    const response = document.querySelector('.response')
+})

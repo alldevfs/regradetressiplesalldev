7d44e91265c61529ac4dd2029fd8fbde0ee4b2dc
alldevfs <alldevfs@outlook.com>
2023-09-02 00:59:36 -0300

    ajutes css, js e html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 document.querySelector('.calc').addEventListener('click', () => {
-    const firstValue = document.querySelector('.firstvalue')
-    const seconValue = document.querySelector('.secondvalue')
-    const thirdValue = document.querySelector('.thirdValue')
-    const forthValue = document.querySelector('.forthValue')
-    const response = document.querySelector('.response')
+    const firstValue = document.querySelector('.firstform').value
+    const seconValue = document.querySelector('.secondform').value
+    const thirdValue = document.querySelector('.thirdform').value
+
+    // verificar se estão vazias
+    if (!firstValue || !seconValue || !thirdValue) {
+        alert(`Informe os três primeiros valores para calcular!`);
+        document.querySelector('.response').value = "invalido";
+        return;
+    } else {
+        // realizar calculo dos valores recebidos e adiciona-lo no campo label de texto.
+        let response = parseFloat((seconValue * thirdValue) / firstValue);
+        document.querySelector('.response').value = response;
+    }
+})
+
+// zerar os campos
+document.querySelector('.reload').addEventListener('click', () => {
+    document.querySelector('.firstform').value = '';
+    document.querySelector('.secondform').value = '';
+    document.querySelector('.thirdform').value = '';
+    document.querySelector('.response').value = '';
 })
